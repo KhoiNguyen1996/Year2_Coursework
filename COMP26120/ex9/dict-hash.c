5e5dd3826313c2fea4c6ec2df8aeb1191c5e26cf
Khoi Minh Nguyen <khoinet9690@gmail.com>
2018-02-28 11:51:06 +0000

    Finished ex9 algorithm

--- COMP26120/ex9/dict-hash.c
+++ COMP26120/ex9/dict-hash.c
@@ -20,7 +20,9 @@ struct table
   cell *cells;
   Table_size table_size; // cell cells [table_size];
   Table_size num_entries; // number of cells in_use
-  int totalCollision;
+
+  double totalCollision;
+  double totalInsert;
   // add anything else that you need
 };
 
@@ -29,10 +31,12 @@ Table initialize_table(Table_size dimension)
 {
   Table newTable = (Table) malloc(sizeof(Table));
   newTable->num_entries = 0;
+  if (!isPrime(dimension) || dimension == 1)
+    dimension = nextPrime(dimension);
   newTable->table_size = dimension;
   newTable->cells = malloc(sizeof(cell) * dimension);
-  newTable->totalCollision = 0;
 
+  newTable->totalCollision = 0;
   return newTable;
 }
 
@@ -73,9 +77,9 @@ int polynomial_hash(const char* key)
 
 // Non zero hash function with formula f(k) = q - (k mod q)
 // q = 7.
-int nonZero_hash(const char* key)
+int nonZero_hash(int key)
 {
-  return 7 - (key % 7);
+  return 17 - (key % 17);
 }
 
 // Compression function that map the key to range 0 to N.
@@ -83,95 +87,306 @@ int compression(int hashKey, Table_size maxRange)
 {
   int a = rand() % maxRange + 1;
   int b = rand() % maxRange + 1;
-  return (int) abs(a * hashKey + b) % maxRange;
+  return (int) (hashKey) % maxRange;
 }
 
 //=============================================================================
-// Probing modes.
-int quadratic_probing(int i, int j, int N)
-{
-  return (i + j * j) % N;
-}
+// Probing modes. Use open addressing.
 
-int linear_probing(int i, int j, int N)
+// Function to insert a value into the table and use linear probing in order to
+// deal with collisions.
+Table linear_probing(Table table, int index, Key_Type value)
 {
-  return (i + j * j) % N;
-}
-
-//=============================================================================
-/*
-Table insert(Key_Type insertKey, Table insertTable)
-{
-  int hashCode = polynomial_hash(insertKey);
-  int hashID = compression(hashCode, insertTable->table_size);
-
-  for (int i = 0; i < insertTable->table_size; i++)
+  int current = table->totalInsert + 1;
+  // For loop to find the index to input the new value into.
+  for (int i = index; i < table->table_size; i++)
   {
-    if (insertTable->cells[quadratic_probing(hashID, i, insertTable->table_size)].state == in_use &&
-        strcmp(insertTable->cells[quadratic_probing(hashID, i, insertTable->table_size)].element, insertKey) == 0)
-      return insertTable;
+    if (table->cells[i].state == in_use &&
+        strcmp(table->cells[i].element, value) == 0)
+      return table;
 
-    else if(insertTable->cells[quadratic_probing(hashID, i, insertTable->table_size)].state == empty)
+    else if(table->cells[i].state == empty)
     {
-      insertTable->cells[quadratic_probing(hashID, i, insertTable->table_size)].state = in_use;
-      insertTable->cells[quadratic_probing(hashID, i, insertTable->table_size)].element = strdup(insertKey);
-      return insertTable;
+      table->cells[i].state = in_use;
+      table->cells[i].element = strdup(value);
+      table->num_entries++;
+      return table;
+    }
+    else
+    {
+      table->totalCollision++;
+      table->totalInsert = current;
     }
   }
-  return insertTable;
-} */
+  return table;
+}
 
+Table quadratic_probing(Table table, int index, Key_Type value)
+{
+  int position;
+  int current = table->totalInsert + 1;
+  // For loop to find the index to input the new value into.
+  for (int i = 0; i < table->table_size; i++)
+  {
+    position = (index + i * i) % table->table_size;
+    if (table->cells[position].state == in_use &&
+        strcmp(table->cells[position].element, value) == 0)
+      return table;
+
+    else if(table->cells[position].state == empty)
+    {
+      table->cells[position].state = in_use;
+      table->cells[position].element = strdup(value);
+      table->num_entries++;
+      return table;
+    }
+    else
+    {
+      table->totalInsert = current;
+      table->totalCollision++;
+    }
+  }
+  return table;
+}
+
+Table double_hashing(Table table, int index, Key_Type value)
+{
+  int current = table->totalInsert + 1;
+  int position;
+
+  // For loop to find the index to input the new value into.
+  for (int i = 0; i < table->table_size; i++)
+  {
+    position = (index + i * nonZero_hash(value)) % table->table_size;
+    if (table->cells[position].state == in_use &&
+        strcmp(table->cells[position].element, value) == 0)
+      return table;
+
+    else if(table->cells[position].state == empty)
+    {
+      table->cells[position].state = in_use;
+      table->cells[position].element = strdup(value);
+      table->num_entries++;
+      return table;
+    }
+    else
+    {
+      table->totalInsert = current;
+      table->totalCollision++;
+    }
+  }
+  return table;
+}
+
+// Get the key of a given value in a table that used linear probing.
+Boolean find_linear(Table table, int index, Key_Type value)
+{
+  // For loop to find the index to input the new value into.
+  for (int i = index; i < table->table_size; i++)
+  {
+    if (table->cells[i].state == in_use &&
+        strcmp(table->cells[i].element, value) == 0)
+      return TRUE;
+  }
+  return FALSE;
+}
+
+// Get the key of a given value in a table that used quadratic probing.
+Boolean find_quadratic(Table table, int index, Key_Type value)
+{
+  int position;
+
+  // For loop to find the index to input the new value into.
+  for (int i = 0; i < table->table_size; i++)
+  {
+    position = (index + i * i) % table->table_size;
+    if (table->cells[position].state == in_use &&
+        strcmp(table->cells[position].element, value) == 0)
+      return TRUE;
+  }
+  return FALSE;
+}
+
+// Get the key of a given value in a table that used double hashing.
+Boolean find_double(Table table, int index, Key_Type value)
+{
+  int position;
+
+  // For loop to find the index to input the new value into.
+  for (int i = 0; i < table->table_size; i++)
+  {
+    position = (index + i * nonZero_hash(value)) % table->table_size;
+    if (table->cells[position].state == in_use &&
+        strcmp(table->cells[position].element, value) == 0)
+      return TRUE;
+  }
+  return FALSE;
+}
+
+//=============================================================================
+// Get table hash ratio.
+double get_ratio(Table table)
+{
+  return (double) table->num_entries / table->table_size;
+}
+
+// Rehash to a larger table of size N is prime.
+Table rehashTable(Table table, int mode)
+{
+  // Setup a new table, then copy the data from the old table.
+  Table newTable = (Table) malloc(sizeof(Table));
+
+  newTable->table_size = nextPrime(table->table_size);
+  newTable->cells = malloc(sizeof(cell) * nextPrime(table->table_size));
+
+  printf("\n=============================================================\n");
+  printf("Rehashing the table into size of: %d\n", newTable->table_size);
+
+  for (int i = 0; i < table->table_size; i++)
+  {
+    if (table->cells[i].state == in_use)
+    {
+      newTable = insert(table->cells[i].element, newTable);
+    }
+  }
+
+  newTable->num_entries = table->num_entries;
+  newTable->totalCollision = table->totalCollision;
+  newTable->totalInsert = table->totalInsert;
+
+  return newTable;
+}
+
+// Get the next prime of a given number.
+int nextPrime(int n)
+{
+    int prime = n*2;
+    while (!isPrime(prime)) prime += 1;
+    return prime;
+}
+
+// Check if a number is a prime.
+int isPrime(int n)
+{
+    int i, startIndex;
+
+    // Check if the number is divisible by 2 or 3.
+    if (n % 2 == 0 || n % 3 == 0)
+      return FALSE;
+
+    startIndex = (int) sqrt(n);
+
+    for (i=5; i<=startIndex; i+=6)
+    {
+        if (n%i == 0)
+           return FALSE;
+    }
+
+    for (i=7; i<=startIndex; i+=6)
+    {
+        if (n%i == 0)
+           return FALSE;
+    }
+
+    // If not divisible by anything return true.
+    return TRUE;
+}
+//=============================================================================
+// Function to insert key, value pair into a table.
 Table insert(Key_Type insertKey, Table insertTable)
 {
+  // Initialise the required variables.
+  int hashID, hashCode;
+
+  // Check if the size of the table is too small, if so rehash.
+  if ((insertTable->num_entries != 0 && get_ratio(insertTable) > 0.5)
+      || get_ratio(insertTable) == 1)
+  {
+    insertTable = rehashTable(insertTable, mode);
+  }
+
   switch(mode)
   {
-    /* Used only summation hash function and use linear probing.
-    */
+    // Mode 1: Used only summation hash function and use linear probing.
     case 1:
-      insertTable = insertValue()
+      hashCode = summation_hash(insertKey);
+      hashID = compression(hashCode, insertTable->table_size);
+      return linear_probing(insertTable, hashID, insertKey);
       break;
 
-    /* Used only summation hash function and use linear probing.
-    */
+    // Mode 2: Used summation hash function and use quadratic probing.
     case 2:
+      hashCode = summation_hash(insertKey);
+      hashID = compression(hashCode, insertTable->table_size);
+      return quadratic_probing(insertTable, hashID, insertKey);
       break;
 
-    /* Used only summation hash function and use linear probing.
-    */
+    // Mode 3: Used polynomial hash function and use linear probing.
     case 3:
+      hashCode = polynomial_hash(insertKey);
+      hashID = compression(hashCode, insertTable->table_size);
+      return linear_probing(insertTable, hashID, insertKey);
       break;
 
-    /* Used only summation hash function and use linear probing.
-    */
+    // Mode 4: Used polynomial hash function and use quadratic probing.
     case 4:
+      hashCode = polynomial_hash(insertKey);
+      hashID = compression(hashCode, insertTable->table_size);
+      return quadratic_probing(insertTable, hashID, insertKey);
       break;
 
-    /* Used only summation hash function and use linear probing.
-    */
+    // Mode default: Used polynomial hash and double hashing.
     default:
+      hashCode = polynomial_hash(insertKey);
+      hashID = compression(hashCode, insertTable->table_size);
+      return double_hashing(insertTable, hashID, insertKey);
       break;
   }
 }
 
-/*
 // Find a key in the table, if in use then true, else false.
 Boolean find(Key_Type findKey, Table findTable)
 {
-  int hashCode = polynomial_hash(findKey);
-  int hashID = compression(hashCode, findTable->table_size);
-  for (int i = 0; i < findTable->table_size; i++)
-  {
-    if(findTable->cells[quadratic_probing(hashID, i, findTable->table_size)].state == in_use &&
-       strcmp(findTable->cells[quadratic_probing(hashID, i, findTable->table_size)].element, findKey) == 0)
-       return 1;
-  }
-  return 0;
-}
-*/
+  // Initialise the required variables.
+  int hashID, hashCode;
 
-// Find a key in the table, if in use then true, else false.
-Boolean find(Key_Type findKey, Table findTable)
-{
+  switch(mode)
+  {
+    // Mode 1: Used only summation hash function and use linear probing.
+    case 1:
+      hashCode = summation_hash(findKey);
+      hashID = compression(hashCode, findTable->table_size);
+      return find_linear(findTable, hashID, findKey);
+      break;
+
+    // Mode 2: Used summation hash function and use quadratic probing.
+    case 2:
+      hashCode = summation_hash(findKey);
+      hashID = compression(hashCode, findTable->table_size);
+      return find_quadratic(findTable, hashID, findKey);
+      break;
+
+    // Mode 3: Used polynomial hash function and use linear probing.
+    case 3:
+      hashCode = polynomial_hash(findKey);
+      hashID = compression(hashCode, findTable->table_size);
+      return find_linear(findTable, hashID, findKey);
+      break;
+
+    // Mode 4: Used polynomial hash function and use quadratic probing.
+    case 4:
+      hashCode = polynomial_hash(findKey);
+      hashID = compression(hashCode, findTable->table_size);
+      return find_quadratic(findTable, hashID, findKey);
+      break;
+
+    // Mode default: Used polynomial hash and double hashing.
+    default:
+      hashCode = polynomial_hash(findKey);
+      hashID = compression(hashCode, findTable->table_size);
+      return find_double(findTable, hashID, findKey);
+      break;
+  }
 }
 
 // Find all the in use cells in the table.
@@ -179,15 +394,21 @@ void print_table(Table printTable)
 {
   for (int i = 0; i < printTable->table_size; i++)
   {
-    if (printTable->cells[i].element != NULL)
+    if (printTable->cells[i].state == in_use)
     {
-      if (printTable->cells[i].state == in_use)
-        printf("Hash ID: %d, Value: %s\n", i ,printTable->cells[i].element);
+      printf("Hash ID: %d, Value: %s\n", i ,printTable->cells[i].element);
     }
   }
 }
 
+// Function to print out any stat related to the table.
 void print_stats(Table statsTable)
 {
+  printf("Total number of collisions: %lf\n", statsTable->totalCollision);
+  printf("Total number of inserts with collisions: %lf\n", statsTable->totalInsert);
+  double collisionsRate = statsTable->totalCollision / statsTable->totalInsert;
+  printf("Collisions per insert: %lf\n", collisionsRate);
 
+  printf("Total number of entries: %d\n", statsTable->num_entries);
+  printf("Current bucket load: %f\n", get_ratio(statsTable));
 }
